e36df902cf8007b15aa5dd4392438174eb4da4a2
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-14 10:07:11 -0600

    color chage of counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   $("#new-tweet").on("input", function () {
     const textarea = $(".upperform").children("textarea");
     const num = textarea.val().length;
-    const newcounter = 140 - num;
-    const counter = $(".lowerform").children("#counter");
-    counter.html(newcounter);
+    let newcounter = 140 - num;
+    let counter = $(".lowerform").children("#counter");
+    if (newcounter < 0) {
+      counter.text(newcounter).css("color", "red");
+    } else {
+      counter.text(newcounter).css("color", "black");
+    }
   });
 });
