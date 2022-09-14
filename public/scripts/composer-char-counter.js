06e676cd4de9cdd58dec499352a6ad761c9890ed
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-14 10:22:00 -0600

    fixed padding onn i cons and spacing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   $("#new-tweet").on("input", function () {
     const textarea = $(".upperform").children("textarea");
     const num = textarea.val().length;
-    let newcounter = 140 - num;
-    let counter = $(".lowerform").children("#counter");
+    const newcounter = 140 - num;
+    const counter = $(".lowerform").children("#counter");
     if (newcounter < 0) {
       counter.text(newcounter).addClass("red");
     } else {
