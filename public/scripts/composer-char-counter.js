12bf9d8b3718c422049b309391adc770a32f9e00
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-15 22:19:27 -0600

    comented code for better experiece

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     const textarea = $(".upperform").children("textarea");
     const num = textarea.val().length;
     const newcounter = 140 - num;
-    const counter = $(".lowerform").children("#counter");
+    const counter = $("#counter");
+
+    /* ~~~~~~~~~ caunter ads class red to alert user limits ~~~~~~~~~~~~~~~~~~ */
     if (newcounter < 0) {
       counter.text(newcounter).addClass("red");
     } else {
