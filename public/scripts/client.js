c3804d6a85e22b3ff01fbee72a650f6e6acdd27a
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-15 21:09:24 -0600

    fixed little bug comming from data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(() => {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const createTweetElement = (data) => {
     const escape = function (str) {
       let div = document.createElement("div");
@@ -64,9 +39,18 @@ $(() => {
     }
   };
 
+  // const loadTweets = () => {
+  //   $.get("/tweets", data).then(() => {
+  //     renderTweets(data);
+  //   });
+  // };
   const loadTweets = () => {
-    $.get("/tweets", data).then((data) => {
-      renderTweets(data);
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      success: (data) => {
+        renderTweets(data);
+      },
     });
   };
 
